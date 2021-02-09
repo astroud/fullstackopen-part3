db475af915739dd55f2b3a17ed3dc3fc3430dd60
Aaron Stroud <astroud@gmail.com>
2021-02-09 09:59:48 -0800

    feat: api returns list of phonebook entries
    3.1: Phonebook backend step1

---
+++ index.js
@@ -0,0 +1,36 @@
+var express = require('express')
+var app = express()
+
+const port = 3001
+
+let persons = [
+  { 
+    "name": "Arto Hellas", 
+    "number": "040-123456",
+    "id": 1
+  },
+  { 
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  { 
+    "name": "Dan Abramov", 
+    "number": "12-43-234345",
+    "id": 3
+  },
+  { 
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122",
+    "id": 4
+  }
+]
+
+
+app.get('/api/persons', (req, res) => {
+  res.send(persons)
+})
+
+app.listen(port, () => {
+  console.log(`Phonebook server listening at http://localhost:${port}`)
+})
