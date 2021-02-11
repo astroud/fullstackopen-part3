0091161881474a7d3ce61acc12bbded4dbbd436a
Aaron Stroud <astroud@gmail.com>
2021-02-11 10:59:37 -0800

    feat: add request logging w/ 'morgan' middleware
    3.7: Phonebook backend step7

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const morgan = require('morgan')
 
+const app = express()
+
+app.use(morgan('tiny'))
 app.use(express.json())
 
 const port = 3001
