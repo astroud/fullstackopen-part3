789fb5db6d1edafda04238e82951832d9f8c7c85
Aaron Stroud <astroud@gmail.com>
2021-02-16 13:59:09 -0800

    feat: command-line tool to +- entries in MongoDB
    3.12: Command-line database

---
+++ mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  console.log('To save an entry: node mongo.js <password> <name> <number>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const newName = process.argv[3]
+const newNumber = process.argv[4]
+
+
+const url =
+  `mongodb+srv://fso:${password}@cluster0.wrp9g.mongodb.net/phonedirectory-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const entrySchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Entry = mongoose.model('Entry', entrySchema)
+
+const entry = new Entry({
+  name: newName,
+  number: newNumber,
+})
+
+if(process.argv[4]){
+  entry.save().then(result => {
+    console.log(`added ${newName} number ${newNumber} to phonebook`)
+    mongoose.connection.close()
+  })
+}
+
+else {
+  Entry.find({}).then(result => {
+    result.forEach(entry => {
+      console.log(`${entry.name} ${entry.number}`)
+    })
+    mongoose.connection.close()
+  })
+}
