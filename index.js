e0f42031a2e2815d71c0a7f01b1459f66e3e11fb
Aaron Stroud <astroud@gmail.com>
2021-02-19 11:28:04 -0800

    feat: Connect backend to database
    3.13: Phonebook database, step1
    3.14: Phonebook database, step2

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
+const { response } = require('express')
 const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-
 const app = express()
+const morgan = require('morgan')
+const Entry = require('./models/entry')
 
-app.use(cors())
+
 app.use(express.static('build'))
 app.use(express.json())
 
@@ -24,84 +25,39 @@ const MORGAN_FORMAT = (tokens, req, res) => {
 
 app.use(morgan(MORGAN_FORMAT))
 
-const port = process.env.PORT || 3001
-
-let persons = [
-  { 
-    "name": "Arto Hellas", 
-    "number": "040-123456",
-    "id": 1
-  },
-  { 
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  { 
-    "name": "Dan Abramov", 
-    "number": "12-43-234345",
-    "id": 3
-  },
-  { 
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122",
-    "id": 4
-  }
-]
-
-const generateId = () => {
-  return Date.now()
-}
 
 app.get('/info', (req, res) => {
-  const entries = persons.length
-
-  res.send(`
-    <p>Phonebook has entries for ${entries} people</p>
+  Entry.countDocuments({}, function (err, count) {
+    res.send(`
+    <p>Phonebook has entries for ${count} people</p>
     <p>${Date()}</p>
-  `)
-  
+    `)
+  });
 })
 
 app.get('/api/persons', (req, res) => {
-  res.send(persons)
+  Entry.find({}).then(entry => {
+    res.json(entry)
+  })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  let entry = persons.filter( person => person.id === id)
-
-  if (entry.length === 0) {
-    return res.status(404).json({ 
-      error: 'person not found' 
-    })
-  }
-  
-  res.json(entry)
+  Entry.findById(req.params.id).then(entry => {
+    res.json(entry)
+  })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const numberOfEntries = persons.length
-
-  persons = persons.filter(person => person.id !== id)
-  
-  if(numberOfEntries === persons.length) {
-    return res.status(404).json({ 
-      error: 'entry already deleted' 
-    })
-  }
-
-  res.json({ 
-    success: 'entry deleted' 
+  Entry.deleteOne({_id: req.params.id}).then(code => {
+    console.log(`Deleting ${req.params.id} !`)
+    res.json(code)
   })
 })
 
 app.post('/api/persons', (req, res) => {
   const body = req.body
-  const existingNames = persons.map(person => person.name.toLowerCase()) 
 
-  if (!body.name) {
+  if (!body.name) { 
     return res.status(400).json({ 
       error: 'name missing' 
     })
@@ -111,18 +67,18 @@ app.post('/api/persons', (req, res) => {
       error: 'phone number missing' 
     })
   }
-  else if (existingNames.includes(body.name.toLowerCase())) {
-    return res.status(400).json({ 
-      error: 'entry already exists with that name' 
-    })
-  }
+  
+  const entry = new Entry({
+    name: body.name,
+    number: body.number,
+  })
 
-  body.id = generateId()
-  persons = persons.concat(body)
-  res.status(200).json({
-    "success": true
+  entry.save().then(savedEntry => {
+    res.json(savedEntry)
   })
 })
+
+const port = process.env.PORT
 
 app.listen(port, () => {
   console.log(`Phonebook server listening at http://localhost:${port}`)
