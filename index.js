cf48998090ff5c302eb85475a6901bb138cef037
Aaron Stroud <astroud@gmail.com>
2021-02-24 11:04:25 -0800

    feat: error handling and expanded req/res names
    3.15: Phonebook database, step3
    3.16: Phonebook database, step4
    3.18*: Phonebook database step6

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ const Entry = require('./models/entry')
 app.use(express.static('build'))
 app.use(express.json())
 
-const MORGAN_FORMAT = (tokens, req, res) => {
-  const body = req.method === 'POST'
-                ? JSON.stringify(req.body)
+const MORGAN_FORMAT = (tokens, request, response) => {
+  const body = request.method === 'POST'
+                ? JSON.stringify(request.body)
                 : ''
   return [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens.res(req, res, 'content-length'), '-',
-    tokens['response-time'](req, res), 'ms',
+    tokens.method(request, response),
+    tokens.url(request, response),
+    tokens.status(request, response),
+    tokens.res(request, response, 'content-length'), '-',
+    tokens['response-time'](request, response), 'ms',
     body
   ].join(' ')
 }
@@ -26,44 +26,59 @@ const MORGAN_FORMAT = (tokens, req, res) => {
 app.use(morgan(MORGAN_FORMAT))
 
 
-app.get('/info', (req, res) => {
-  Entry.countDocuments({}, function (err, count) {
-    res.send(`
+app.get('/info', (request, response) => {
+  Entry.countDocuments({}, (error, count) => {
+    if(error) {
+      next(error)
+    }
+
+    response.send(`
     <p>Phonebook has entries for ${count} people</p>
     <p>${Date()}</p>
     `)
   });
 })
 
-app.get('/api/persons', (req, res) => {
-  Entry.find({}).then(entry => {
-    res.json(entry)
-  })
+app.get('/api/persons', (request, response, next) => {
+  Entry.find({})
+    .then(entry => {
+      response.json(entry)
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  Entry.findById(req.params.id).then(entry => {
-    res.json(entry)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Entry.findById(request.params.id)
+    .then(entry => {
+      if(entry) {
+        response.json(entry)
+      }
+      else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  Entry.deleteOne({_id: req.params.id}).then(code => {
-    console.log(`Deleting ${req.params.id} !`)
-    res.json(code)
-  })
+app.delete('/api/persons/:id', (request, response, next) => {
+  Entry.deleteOne({_id: request.params.id})
+    .then(code => {
+      console.log(`Deleting ${request.params.id} !`)
+      response.json(code)
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
-  const body = req.body
+app.post('/api/persons', (request, response) => {
+  const body = request.body
 
   if (!body.name) { 
-    return res.status(400).json({ 
+    return response.status(400).json({ 
       error: 'name missing' 
     })
   }
   else if (!body.number) {
-    return res.status(400).json({ 
+    return response.status(400).json({ 
       error: 'phone number missing' 
     })
   }
@@ -74,9 +89,27 @@ app.post('/api/persons', (req, res) => {
   })
 
   entry.save().then(savedEntry => {
-    res.json(savedEntry)
+    response.json(savedEntry)
   })
 })
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const port = process.env.PORT
 
